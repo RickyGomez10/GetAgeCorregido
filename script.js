fe28cd0e7b2b33b8cdd53455145932a0cfd8d2b4
RickyGomez10 <00108517@uca.edu.sv>
2018-10-26 01:13:26 -0600

    Arreglada la funcion getage para que tome meses

---
+++ script.js
@@ -0,0 +1,74 @@
+// Object Literal
+var person = {
+    /* property: value*/
+    name: "Néstor",
+    lastname: "Aldana",
+    birthday: Date.now()
+}; // JSON (JavaScript Object Notation)
+
+// Access to propertys
+console.log(person.name);
+// Change object's property value
+person.birthday = new Date(2000, 9, 28); // 11 - Jan - 1994
+console.log(person.birthday);
+
+console.log(person.dui) // undefined
+person.dui = "000000000" // Assign 
+console.log(person.dui) // 000000000
+
+// Example
+function createPerson(name, lastname, birthday, dui) {
+    return {
+        name,
+        lastname,
+        birthday,
+        dui
+    }
+}
+
+let list = [] // To save persons
+
+// To Add 10 fake persons
+for (let i = 0; i < 10; i++) {
+    list.push(createPerson(`Name ${i}`, `Lastname ${i}`, new Date().setFullYear(1990 + i + Math.floor(Math.random() * 5)), `000000${i}`));
+}
+
+console.table(list);
+
+// Array Higher function
+// Show only the name persons
+console.table(list.map(({
+    name
+}) => name));
+
+// Get average age
+console.log("Average age %i", list.reduce((sum, {
+    birthday
+}) => {
+    var ageDifMs = Date.now() - birthday;
+    var ageDate = new Date(ageDifMs); // miliseconds from epoch
+    return Math.abs(ageDate.getUTCFullYear() - 1970) + sum;
+}, 0) / list.length);
+
+
+// More readable
+
+/* Funcion para obtener edad evaluando meses incluso*/
+function getAge(dateString) {
+    var today = new Date();
+    var birthDate = new Date(dateString);
+    var age = today.getFullYear() - birthDate.getFullYear();
+    var m = today.getMonth() - birthDate.getMonth();
+    if (m < 0 || (m === 0 && today.getDate() < birthDate.getDate())) {
+        age--;
+    }
+    return age;
+}
+
+
+console.log("Average age %i", list.reduce((sum, {
+    birthday
+}) => getAge(birthday) + sum, 0) / list.length);
+
+/* Mostrando la edad de la persona*/
+console.log("Su edad es: "+ getAge(person.birthday))
